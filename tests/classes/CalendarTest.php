1995af32daedd3b11c5fb8694ee35271ca341625
jhoughtelin <josh@findsomehelp.com>
2015-06-02 13:06:38 -0600

    Jenkins configuration has been established.

--- tests/classes/CalendarTest.php
+++ tests/classes/CalendarTest.php
@@ -1,5 +1,5 @@
 <?php
-
+date_default_timezone_set('UTC');
 /**
  * @file CalendarTest.php
  * @project VRPConnector
--- tests/classes/VRPConnectorTest.php
+++ tests/classes/VRPConnectorTest.php
@@ -5,8 +5,10 @@
  * @author Josh Houghtelin <josh@findsomehelp.com>
  * @created 2/19/15 6:13 PM
  */
+date_default_timezone_set('UTC');
 
-class VRPConnectorTest extends PHPUnit_Framework_TestCase {
+class VRPConnectorTest extends PHPUnit_Framework_TestCase
+{
     public function testWillPass()
     {
         $this->assertTrue(true);
