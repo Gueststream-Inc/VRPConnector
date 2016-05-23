0985ba0f7b6526014d11bd29b694a641c93db518
jhoughtelin <josh@findsomehelp.com>
2016-05-23 10:37:22 -0600

    Fixing the form field names to use the correct names for submission to the API

--- themes/mountainsunset/vrpSubmitReview.php
+++ themes/mountainsunset/vrpSubmitReview.php
@@ -115,17 +115,21 @@
 </style>
 
 <div id="vrp">
+
     <h1>Submit a Review</h1>
 
+    <p id="vrp-add-review-success" style="display:none;">Thank you for submitting your review.  It will be processed shortly!</p>
 
     <form id="vrpSubmitReviewForm">
+        <input type="hidden" name="source" value="website" />
+
         <!-- Property Select -->
         <div class="vrp-col-md-6">
             <label for="unitId">Property you are reviewing</label>
-            <select name="unitId" id="unitId">
+            <select name="prop_id" id="unitId" required="" >
                 <option value=""></option>
                 <?php foreach ($data['allUnits'] as $unit) : ?>
-                    <option><?php echo $unit->Name; ?></option>
+                    <option value="<?php echo $unit->id;?>"><?php echo $unit->Name; ?></option>
                 <?php endforeach; ?>
             </select>
         </div>
@@ -133,7 +137,7 @@
         <!-- Date of Arrival -->
         <div class="vrp-col-md-6">
             <label for="arrivalDate">Approx. Date of Arrival</label>
-            <input type="text" name="arrivalDate" id="arrivalDate" readonly/>
+            <input type="text" name="checkin_date" id="arrivalDate" required="" readonly/>
         </div>
 
         <!-- Overall Rating of Property -->
@@ -141,13 +145,13 @@
             Overall Property Rating
             <fieldset>
                 <span class="star-cb-group">
-                  <input type="radio" id="rating-5" name="overallRating" value="5"/><label for="rating-5">5</label>
-                  <input type="radio" id="rating-4" name="overallRating" value="4"/><label for="rating-4">4</label>
-                  <input type="radio" id="rating-3" name="overallRating" value="3"/><label for="rating-3">3</label>
-                  <input type="radio" id="rating-2" name="overallRating" value="2"/><label for="rating-2">2</label>
-                  <input type="radio" id="rating-1" name="overallRating" value="1"/><label for="rating-1">1</label>
-                  <input type="radio" id="rating-0" name="overallRating" value="0" class="star-cb-clear"
-                         checked="checked"/><label for="rating-0">0</label>
+                  <input type="radio" id="rating-5" name="rating" value="5"/><label for="rating-5">5</label>
+                  <input type="radio" id="rating-4" name="rating" value="4"/><label for="rating-4">4</label>
+                  <input type="radio" id="rating-3" name="rating" value="3"/><label for="rating-3">3</label>
+                  <input type="radio" id="rating-2" name="rating" value="2"/><label for="rating-2">2</label>
+                  <input type="radio" id="rating-1" name="rating" value="1"/><label for="rating-1">1</label>
+                  <input type="radio" id="rating-0" name="rating" value="0" class="star-cb-clear"
+                         checked="checked" required=""/><label for="rating-0">0</label>
                 </span>
             </fieldset>
         </div>
@@ -155,13 +159,13 @@
         <!-- Review Name -->
         <div class="vrp-col-md-12">
             <label for="reviewName">Title of your review</label>
-            <input type="text" name="reviewName" id="revieName">
+            <input type="text" name="title" id="reviewName" required="" />
         </div>
 
         <!-- Review Description -->
         <div class="vrp-col-md-12">
             <label for="review-description">Your review</label>
-            <textarea name="reviewDescription" id="review-description" maxlength="250"></textarea>
+            <textarea name="review" id="review-description" maxlength="250" required=""></textarea>
             <div class="max-char-notice">(250 Characters Max)</div>
         </div>
 
@@ -170,21 +174,24 @@
         </div>
         <div class="vrp-col-md-12">
             Email Address <i>(Will not be displayed)</i>
-            <input type="email" name="emailAddress" required=""/>
+            <input type="email" name="email" required="" />
         </div>
 
         <div class="vrp-col-md-12">
             Name <i>(Will be displayed)</i>
-            <input type="text" name="guestName" required=""/>
+            <input type="text" name="name" required="" />
         </div>
 
         <div class="vrp-col-md-12">
             Where I Live <i>(City, State/Region, Country)</i>
-            <input type="text" name="guestAddress">
+            <input type="text" name="guest_location" required="" />
         </div>
 
         <div class="vrp-col-md-12 review-submit">
-            <button type="submit" class="vrp-btn">Submit Review</button>
+            <button type="submit"
+                    class="vrp-btn"
+                    id="vrp-btn-add-review"
+                    data-sending-label="Sending">Submit Review</button>
         </div>
 
     </form>
@@ -342,6 +349,28 @@
             log.html(me.attr('value'));
         });
 
+        jQuery(document).ready(function () {
+            jQuery('#review_check_in').datepicker({});
+
+            jQuery("#vrpSubmitReviewForm").submit(function(event){
+                event.preventDefault();
+                jQuery("#vrp-btn-add-review").attr("disabled","disabled");
+                jQuery.post(url_paths.site_url + "/?vrpjax=1&act=addReview",jQuery(this).serialize(),function(response) {
+                    if(response.success == true) {
+                        jQuery("#vrp-add-review-success").show();
+                        jQuery("#vrpSubmitReviewForm").hide();
+                    }
+                    if(response.success == false) {
+                        response.errors.forEach(function (element, index, array) {
+                            alert(element);
+                        });
+                        jQuery("#vrp-btn-add-review").attr("disabled",false);
+                    }
+                    console.log(response);
+                });
+                return false;
+            });
+        });
 
     })(jQuery);
 
