2e3f3e3d9e5c581c0ca41aa8833ddc8d557d7ae4
jhoughtelin <josh@findsomehelp.com>
2016-09-23 16:12:48 -0600

    CSS/Styling cleanup & script/css seperation

--- themes/mountainsunset/js/vrp.submitReview.js
+++ themes/mountainsunset/js/vrp.submitReview.js
@@ -1,26 +1,51 @@
 /**
  * [vrpSubmitReview] Shortcode JavaScript
  */
+(function (jQuery) {
+    'use strict';
 
-jQuery(document).ready(function () {
-    jQuery('#review_check_in').datepicker({});
+    jQuery(document).ready(function () {
+        var vrpSubmitReviewForm = jQuery("#vrpSubmitReviewForm");
 
-    jQuery("#vrpSubmitReviewForm").submit(function(event){
-        event.preventDefault();
-        jQuery("#vrp-btn-add-review").attr("disabled","disabled");
-        jQuery.post(url_paths.site_url + "/?vrpjax=1&act=addReview",jQuery(this).serialize(),function(response) {
-            if(response.success == true) {
-                jQuery("#vrp-add-review-success").show();
-                jQuery("#vrp-add-review-form").hide();
-            }
-            if(response.success == false) {
-                response.errors.forEach(function (element, index, array) {
-                    alert(element);
+        if (vrpSubmitReviewForm.length) {
+            // Autocomplete Property Selection
+
+            jQuery("#toggle").click(function () {
+                jQuery("#unitId").toggle();
+            });
+
+            // Arrival Date
+            jQuery("#arrivalDate").datepicker({});
+
+            jQuery('#review_check_in').datepicker({
+                dateFormat: 'yy-mm-dd'
+            });
+
+            vrpSubmitReviewForm.submit(function (event) {
+
+                event.preventDefault();
+
+                jQuery("#vrp-btn-add-review").attr("disabled", "disabled");
+
+                jQuery.post(url_paths.site_url + "/?vrpjax=1&act=addReview", jQuery(this).serialize(), function (response) {
+
+                    if (response.success == true) {
+                        jQuery("#vrp-add-review-success").show();
+                        vrpSubmitReviewForm.hide();
+                    }
+
+                    if (response.success == false) {
+                        response.errors.forEach(function (element, index, array) {
+                            alert(element);
+                        });
+                        jQuery("#vrp-btn-add-review").attr("disabled", false);
+                    }
+
                 });
-                jQuery("#vrp-btn-add-review").attr("disabled",false);
-            }
-            console.log(response);
-        });
-        return false;
+
+                return false;
+            });
+        }
     });
-});
+
+})(jQuery);
--- themes/mountainsunset/vrpSubmitReview.php
+++ themes/mountainsunset/vrpSubmitReview.php
@@ -2,127 +2,23 @@
 /**
  * [vrpSubmitReview] shortcode VRPConnector Theme File
  *
- * @injected $data['allUnits']
+ * In order to use this shortcode the review module must be enabled in the VRP.
+ *
+ * @since 1.4.0
+ * @var $data['allUnits']
  */
 ?>
-<style>
-
-    .star-cb-group {
-        font-size: 0;
-        unicode-bidi: bidi-override;
-        direction: rtl;
-    }
-
-    .star-cb-group * {
-        font-size: 4rem;
-    }
-
-    .star-cb-group > input {
-        display: none;
-    }
-
-    .star-cb-group > input + label {
-        display: inline-block;
-        overflow: hidden;
-        text-indent: 9999px;
-        width: 1em;
-        white-space: nowrap;
-        cursor: pointer;
-    }
-
-    .star-cb-group > input + label:before {
-        display: inline-block;
-        text-indent: -9999px;
-        content: "☆";
-        color: #888;
-    }
-
-    .star-cb-group > input:checked ~ label:before, .star-cb-group > input + label:hover ~ label:before, .star-cb-group > input + label:hover:before {
-        content: "★";
-        color: #e52;
-        text-shadow: 0 0 1px #333;
-    }
-
-    .star-cb-group > .star-cb-clear + label {
-        text-indent: -9999px;
-        width: .5em;
-        margin-left: -.5em;
-    }
-
-    .star-cb-group > .star-cb-clear + label:before {
-        width: .5em;
-    }
-
-    .star-cb-group:hover > input + label:before {
-        content: "☆";
-        color: #888;
-        text-shadow: none;
-    }
-
-    .star-cb-group:hover > input + label:hover ~ label:before, .star-cb-group:hover > input + label:hover:before {
-        content: "★";
-        color: #e52;
-        text-shadow: 0 0 1px #333;
-    }
-
-    #vrpSubmitReviewForm fieldset {
-        padding: 0;
-        margin: 0;
-        border: 0;
-    }
-
-    .custom-combobox {
-        position: relative;
-        display: inline-block;
-    }
-
-    .custom-combobox-toggle {
-        position: absolute;
-        top: 0;
-        bottom: 0;
-        margin-left: -1px;
-        padding: 0;
-    }
-
-    .custom-combobox-input {
-        margin: 0;
-        padding: 5px 10px;
-    }
-
-    #vrpSubmitReviewForm i {
-        color: #676767;
-    }
-
-    #vrpSubmitReviewForm h4.review-section-header {
-        margin:20px 0 10px 0;
-        border-top: 1px dashed #676767;
-        padding-top: 10px;
-    }
-
-    #vrpSubmitReviewForm #review-description {
-        min-height:125px;
-    }
-
-    #vrpSubmitReviewForm .max-char-notice {
-        text-align: right;
-        font-size: .7em;
-    }
-
-    #vrpSubmitReviewForm .review-submit {
-        text-align: right;
-        padding: 5px;
-    }
-</style>
-
 <div id="vrp">
 
-    <p id="vrp-add-review-success" style="display:none;">Thank you for submitting your review.  It will be processed shortly!</p>
+    <p id="vrp-add-review-success" style="display:none;">
+        Thank you for submitting your review.  It will be processed shortly!
+    </p>
 
     <form id="vrpSubmitReviewForm">
         <input type="hidden" name="source" value="website" />
 
         <!-- Property Select -->
-        <div class="vrp-col-md-6">
+        <div class="vrp-col-md-12">
             <label for="unitId">Property you are reviewing</label>
             <select name="prop_id" id="unitId" required="" >
                 <option value=""></option>
@@ -133,7 +29,7 @@
         </div>
 
         <!-- Date of Arrival -->
-        <div class="vrp-col-md-6">
+        <div class="vrp-col-md-12">
             <label for="arrivalDate">Approx. Date of Arrival</label>
             <input type="text" name="checkin_date" id="arrivalDate" required="" readonly/>
         </div>
@@ -152,6 +48,7 @@
                          checked="checked" required=""/><label for="rating-0">0</label>
                 </span>
             </fieldset>
+            <br style="clear:both;"/>
         </div>
 
         <!-- Review Name -->
@@ -170,6 +67,7 @@
         <div class="vrp-col-md-12">
             <h4 class="review-section-header">My Information</h4>
         </div>
+
         <div class="vrp-col-md-12">
             Email Address <i>(Will not be displayed)</i>
             <input type="email" name="email" required="" />
@@ -195,184 +93,3 @@
     </form>
 
 </div>
-
-
-<script>
-    (function ($) {
-        $.widget("custom.combobox", {
-            _create: function () {
-                this.wrapper = $("<span>")
-                    .addClass("custom-combobox")
-                    .insertAfter(this.element);
-
-                this.element.hide();
-                this._createAutocomplete();
-                this._createShowAllButton();
-            },
-
-            _createAutocomplete: function () {
-                var selected = this.element.children(":selected"),
-                    value = selected.val() ? selected.text() : "";
-
-                this.input = $("<input>")
-                    .appendTo(this.wrapper)
-                    .val(value)
-                    .attr("title", "")
-                    .addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left")
-                    .autocomplete({
-                        delay: 0,
-                        minLength: 0,
-                        source: $.proxy(this, "_source")
-                    })
-                    .tooltip({
-                        tooltipClass: "ui-state-highlight"
-                    });
-
-                this._on(this.input, {
-                    autocompleteselect: function (event, ui) {
-                        ui.item.option.selected = true;
-                        this._trigger("select", event, {
-                            item: ui.item.option
-                        });
-                    },
-
-                    autocompletechange: "_removeIfInvalid"
-                });
-            },
-
-            _createShowAllButton: function () {
-                var input = this.input,
-                    wasOpen = false;
-
-                $("<a>")
-                    .attr("tabIndex", -1)
-                    .attr("title", "Show All Properties")
-                    .tooltip()
-                    .appendTo(this.wrapper)
-                    .button({
-                        icons: {
-                            primary: "ui-icon-triangle-1-s"
-                        },
-                        text: false
-                    })
-                    .removeClass("ui-corner-all")
-                    .addClass("custom-combobox-toggle ui-corner-right")
-                    .mousedown(function () {
-                        wasOpen = input.autocomplete("widget").is(":visible");
-                    })
-                    .click(function () {
-                        input.focus();
-
-                        // Close if already visible
-                        if (wasOpen) {
-                            return;
-                        }
-
-                        // Pass empty string as value to search for, displaying all results
-                        input.autocomplete("search", "");
-                    });
-            },
-
-            _source: function (request, response) {
-                var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
-                response(this.element.children("option").map(function () {
-                    var text = $(this).text();
-                    if (this.value && ( !request.term || matcher.test(text) ))
-                        return {
-                            label: text,
-                            value: text,
-                            option: this
-                        };
-                }));
-            },
-
-            _removeIfInvalid: function (event, ui) {
-
-                // Selected an item, nothing to do
-                if (ui.item) {
-                    return;
-                }
-
-                // Search for a match (case-insensitive)
-                var value = this.input.val(),
-                    valueLowerCase = value.toLowerCase(),
-                    valid = false;
-                this.element.children("option").each(function () {
-                    if ($(this).text().toLowerCase() === valueLowerCase) {
-                        this.selected = valid = true;
-                        return false;
-                    }
-                });
-
-                // Found a match, nothing to do
-                if (valid) {
-                    return;
-                }
-
-                // Remove invalid value
-                this.input
-                    .val("")
-                    .attr("title", value + " didn't match any item")
-                    .tooltip("open");
-                this.element.val("");
-                this._delay(function () {
-                    this.input.tooltip("close").attr("title", "");
-                }, 2500);
-                this.input.autocomplete("instance").term = "";
-            },
-
-            _destroy: function () {
-                this.wrapper.remove();
-                this.element.show();
-            }
-        });
-    })(jQuery);
-
-    (function ($) {
-        // Autocomplete Property Selection
-        $("#unitId").combobox();
-        $("#toggle").click(function () {
-            $("#unitId").toggle();
-        });
-
-        // Arrival Date
-        $("#arrivalDate").datepicker({});
-
-        // Star Rating
-        var logID = 'log',
-            log = $('<div id="' + logID + '"></div>');
-        $('body').append(log);
-        $('[type*="radio"]').change(function () {
-            var me = $(this);
-            log.html(me.attr('value'));
-        });
-
-        jQuery(document).ready(function () {
-            jQuery('#review_check_in').datepicker({
-                dateFormat: 'yy-mm-dd'
-            });
-
-            jQuery("#vrpSubmitReviewForm").submit(function(event){
-                event.preventDefault();
-                jQuery("#vrp-btn-add-review").attr("disabled","disabled");
-                jQuery.post(url_paths.site_url + "/?vrpjax=1&act=addReview",jQuery(this).serialize(),function(response) {
-                    if(response.success == true) {
-                        jQuery("#vrp-add-review-success").show();
-                        jQuery("#vrpSubmitReviewForm").hide();
-                    }
-                    if(response.success == false) {
-                        response.errors.forEach(function (element, index, array) {
-                            alert(element);
-                        });
-                        jQuery("#vrp-btn-add-review").attr("disabled",false);
-                    }
-                    console.log(response);
-                });
-                return false;
-            });
-        });
-
-    })(jQuery);
-
-
-</script>
