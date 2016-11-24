f755074b50f71cb6a4c9c290fb81a6e0e72c9d40
jhoughtelin <josh@findsomehelp.com>
2016-11-24 13:40:35 -0700

    WordPress code style standards for specials template

--- themes/mountainsunset/specials.php
+++ themes/mountainsunset/specials.php
@@ -1,28 +1,30 @@
-<h1>Specials</h1>
 <?php
-echo '<pre>';
-print_r( $data );
-echo '</pre>';
-?>
+/**
+ * [vrpSpecials] Template
+ *
+ * @package VRPConnector
+ */
 
-<?php if ( count( $data ) == 0 ) : ?>
+?>
+<h1>Specials</h1>
+<?php if ( 0 === count( $data ) ) : ?>
 	<p>There are no current specials. Please check back soon.</p>
 <?php else : ?>
 	<?php foreach ( $data as $special ) : ?>
 		<div class="sbox">
 			<div class="vrpcontainer_12">
 				<div class="vrpgrid_3">
-					<?php if ( $special->image != '' ) : ?>
-						<img src="<?php echo $special->image; ?>" width="185px" class="simg">
+					<?php if ( ! empty( $special->image ) ) : ?>
+						<img src="<?php echo esc_url( $special->image ); ?>" width="185px" class="simg">
 					<?php endif; ?>
 				</div>
 			</div>
 			<div class="vrpcontainer_12">
 				<div class="vrpgrid_9">
-					<b><?php echo $special->title; ?></b>
+					<b><?php echo esc_html( $special->title ); ?></b>
 					<br>
 					<br/>
-					<?php echo $special->content; ?>
+					<?php echo esc_html( $special->content ); ?>
 				</div>
 			</div>
 
